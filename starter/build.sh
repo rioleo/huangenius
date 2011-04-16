136f672870718879db013bb8cd29a6daf3b716c3
Joseph Hilpuan Lau <jhlau@corn13.stanford.edu>
2011-04-16 00:47:40 -0700

    Part 1 jaccard

--- starter/build.sh
+++ starter/build.sh
@@ -4,6 +4,6 @@ if [ ! -e bin/ ]; then
   mkdir bin
 fi
 
-javac -cp lib/lucene-core-2.9.0.jar -source 1.5 -target 1.5 \
+javac -Xlint:unchecked -cp lib/lucene-core-2.9.0.jar -source 1.5 -target 1.5 \
   -d bin/ `find src/ -name *.java`
 
--- starter/src/cs276/pe1/spell/KGramSpellingCorrector.java
+++ starter/src/cs276/pe1/spell/KGramSpellingCorrector.java
@@ -5,20 +5,97 @@ import java.util.List;
 
 import cs276.util.IOUtils;
 import cs276.util.StringUtils;
+import java.util.*;
+
+import cs276.util.Counter;
 
 public class KGramSpellingCorrector implements SpellingCorrector {
 	/** Initializes spelling corrector by indexing kgrams in words from a file */
+	
+	private HashMap<String, Counter<String>> index;
+	
 	public KGramSpellingCorrector() {
-        File path = new File("/afs/ir/class/cs276/pe1-2009/big.txt.gz");
+	    
+	    // instantiate new index
+	    index = new HashMap<String, Counter<String>>();
+	    
+        File path = new File("/afs/ir/class/cs276/pe1-2011/big.txt.gz");
         for (String line : IOUtils.readLines(IOUtils.openFile(path))) {
             for (String word : StringUtils.tokenize(line)) {
-            	// TODO do kgram indexing
+                ArrayList<String> bigrams = getBigrams(word);
+                
+                for (String bigram : bigrams) {
+                    if (index.containsKey(bigram)) {
+                        index.get(bigram).incrementCount(word);
+                    } else {
+                        Counter<String> words = new Counter<String>();
+                        words.incrementCount(word);
+                        index.put(bigram, words);
+                    }
+                }
+                
+//                for (String key : index.keySet()) {
+//                    System.out.println(key + " " + index.get(key));
+//                }
+                
             }
         }
 	}
 
 	public List<String> corrections(String word) {
-		// TODO fill in method here
-		return null;
+		Set<String> wordBigrams = getBigramsSet(word);
+		
+		Counter<String> possibleCorrections = new Counter<String>();
+		
+		for (String wordBigram : wordBigrams) {
+		    Set<String> postings = null;
+		    if (index.containsKey(wordBigram)) postings = index.get(wordBigram).keySet();
+		    if (postings != null) {
+		        for (String posting : postings) {
+		            if (!possibleCorrections.containsKey(posting)) {
+	                    Set<String> intersect = getBigramsSet(posting);
+                        intersect.retainAll(wordBigrams);
+	                    
+	                    Set<String> union = getBigramsSet(posting);
+	                    union.addAll(wordBigrams);
+	                    
+//	                    System.out.println(word + " " + posting + ": " + intersect);
+	                    
+	                    possibleCorrections.setCount(posting, ((double) intersect.size())/union.size());
+		                
+	                }
+		        }
+		    }
+		}
+		
+	    System.out.println("Word: " + word);
+	    for (String entry : possibleCorrections.topK(10)) {
+	        System.out.println(entry + " " + possibleCorrections.getCount(entry));
+	    }
+		
+		return possibleCorrections.topK(10);
+		
 	}
+	
+	
+	private Set<String> getBigramsSet(String word) {
+	    Set<String> bigrams = new HashSet<String>();
+	    word = "$" + word + "$";
+        for (int i = 0; i < word.length()-1; i++) {
+            String bigram = word.substring(i, i+2);
+            bigrams.add(bigram);
+        }
+        return bigrams;
+	}
+	
+	private ArrayList<String> getBigrams(String word) {
+	    ArrayList<String> bigrams = new ArrayList<String>();
+	    word = "$" + word + "$";
+        for (int i = 0; i < word.length()-1; i++) {
+            String bigram = word.substring(i, i+2);
+            bigrams.add(bigram);
+        }
+        return bigrams;
+	}
+	
 }

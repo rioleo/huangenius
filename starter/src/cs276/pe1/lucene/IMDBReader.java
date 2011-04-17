7e1fa13530eac3fa2a97d6c2eb6b8ddb6adeb0bc
Joseph Hilpuan Lau <jhlau@corn09.stanford.edu>
2011-04-17 04:12:30 -0700

    Queries

--- starter/src/cs276/pe1/lucene/IMDBReader.java
+++ starter/src/cs276/pe1/lucene/IMDBReader.java
@@ -18,36 +18,59 @@ public class IMDBReader {
 	public static void main(String[] argv) throws Exception {
 
 
-		// Access the file
 		File indexPath = new File(new File(System.getProperty("user.home")),"cs276-index");		
-		IndexReader ireader = IndexReader.open(indexPath, true);
+		IndexReader ireader = IndexReader.open(indexPath);
 		
 		// Is there stuff in it?
-		System.out.println(ireader.numDocs());
+		System.out.println("Total docs: " + ireader.numDocs());
+		
+//		System.out.println(ireader.document(100));
+		
 		IndexSearcher indexsearcher = new IndexSearcher(ireader);
 		
-		// Search within the title field
-		//QueryParser queryparser = new QueryParser("author", new StandardAnalyzer());
-		
-		// Search for items that Rob has written
-		//Query query = queryparser.parse("Rob");
-		Term term = new Term("authors", "Rob");
 
-		PhraseQuery query= new PhraseQuery();
-		query.add(term);
-		query.setSlop(0);
+		
+
+
+// 10 items query works
+//        Term term = new Term("title", "\"10 Items or Less\"");
+//		PrefixQuery query = new PrefixQuery(term);
+
+// Murdered eighteen query DOES NOT WORK
+//        Term term1 = new Term("plots", "murdered");
+//        Term term2 = new Term("plots", "eighteen");
+//        PhraseQuery query = new PhraseQuery();
+//        query.add(term1);
+//        query.add(term2);
+//        query.setSlop(5);
+
+
+// Rob query WORKS
+		Term term = new Term("authors", "Rob ");
+        TermQuery query = new TermQuery(term);		
+
+
+		TopDocs results = indexsearcher.search(query, null, 20);
+
+		
+		for (ScoreDoc doc : results.scoreDocs) {
+		    System.out.println(ireader.document(doc.doc));
+	    }
 		
 		
-		Hits hits = indexsearcher.search(query);
-		int hitCount = hits.length();
-		System.out.println(hitCount);
- 		for (int i = 0; i < hitCount; i++) {
-                Document doc = hits.doc(i);
-                System.out.println("  " + (i + 1) + ". " + doc.get("title"));
-            }
-		
+//		Hits hits = indexsearcher.search(query);
+//		int hitCount = hits.length();
+//		System.out.println("Results: " + hitCount);
+// 		for (int i = 0; i < hitCount; i++) {
+//                Document doc = hits.doc(i);
+//                System.out.println("  " + (i + 1) + ". " + doc.get("title"));
+//            }
+//		
 		//Hits hits = indexsearcher.search(query);
-		System.err.println("Done");
+        System.out.println("Query: " + query);
+		System.out.println("Results: " + results.totalHits);
+//		System.err.println("Done");
+	
 	}
 	
 	

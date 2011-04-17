e7cb2784d7416e0c26bcc1126e362e0c18657cf3
Rio Akasaka <rio@rioleo.org>
2011-04-16 22:21:19 -0700

    Changed to Terms

--- starter/src/cs276/pe1/lucene/IMDBReader.java
+++ starter/src/cs276/pe1/lucene/IMDBReader.java
@@ -11,23 +11,33 @@ import cs276.pe1.lucene.IMDBParser.MoviePlotRecord;
 import org.apache.lucene.document.*;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
 
 public class IMDBReader {
 	
 	public static void main(String[] argv) throws Exception {
-		// where to store the index: you can choose your own location,
-		// but be sure it is not in your pe1 folder (it should not be
-		// submitted along with your code).  the lines below create
-		// a new folder cs276-index in your home directory.
-		File indexPath = new File(new File(System.getProperty("user.home")),"cs276-index");
-		//indexPath.mkdir();
-		
-		
+
+
+		// Access the file
+		File indexPath = new File(new File(System.getProperty("user.home")),"cs276-index");		
 		IndexReader ireader = IndexReader.open(indexPath, true);
+		
+		// Is there stuff in it?
 		System.out.println(ireader.numDocs());
 		IndexSearcher indexsearcher = new IndexSearcher(ireader);
-		QueryParser queryparser = new QueryParser("authors", new StandardAnalyzer());
-		Query query = queryparser.parse("a");
+		
+		// Search within the title field
+		//QueryParser queryparser = new QueryParser("author", new StandardAnalyzer());
+		
+		// Search for items that Rob has written
+		//Query query = queryparser.parse("Rob");
+		Term term = new Term("authors", "Rob");
+
+		PhraseQuery query= new PhraseQuery();
+		query.add(term);
+		query.setSlop(0);
+		
+		
 		Hits hits = indexsearcher.search(query);
 		int hitCount = hits.length();
 		System.out.println(hitCount);

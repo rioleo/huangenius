183018b48a2d264a4d61352c6e652291e854e2b7
Joseph Hilpuan Lau <jhlau@corn23.stanford.edu>
2011-04-18 02:14:08 -0700

    Spellcheckers

--- starter/src/cs276/pe1/lucene/IMDBReader.java
+++ starter/src/cs276/pe1/lucene/IMDBReader.java
@@ -12,8 +12,41 @@ import org.apache.lucene.document.*;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
+import java.util.List;
+
+import cs276.pe1.spell.KGramWithEditDistanceSpellingCorrector;
 
 public class IMDBReader {
+	
+	static KGramWithEditDistanceSpellingCorrector spellChecker = new KGramWithEditDistanceSpellingCorrector();
+	
+	public static String runQueryForTitle(String rawQuery) throws Exception {
+	
+		File indexPath = new File(new File(System.getProperty("user.home")),"cs276-index");		
+		IndexReader ireader = IndexReader.open(indexPath);
+		IndexSearcher indexsearcher = new IndexSearcher(ireader);
+
+        QueryParser queryParser = new QueryParser("title",new StandardAnalyzer());
+        List<String> corrections = spellChecker.corrections(rawQuery);
+        if (corrections != null && corrections.size() > 0 && !corrections.get(0).equals(rawQuery)) {
+            rawQuery = corrections.get(0);
+            System.out.println("******Spellchecker: searching for " + rawQuery);
+        }
+        
+        Query query = queryParser.parse(rawQuery);
+	
+		TopDocs results = indexsearcher.search(query, null, 20);
+		for (ScoreDoc doc : results.scoreDocs) {
+		    System.out.println(ireader.document(doc.doc).get("title"));
+	    }
+		
+		
+        System.out.println("Query: " + query);
+		System.out.println("Results: showing " + results.scoreDocs.length + " out of " + results.totalHits);
+        return "";
+	
+	}
+	
 	
 	public static void main(String[] argv) throws Exception {
 
@@ -32,14 +65,14 @@ public class IMDBReader {
 		
 
 
-// 10 items query works this no longer works
-//Term term = new Term("title", "\"10 Items or Less\"");
-//PrefixQuery query = new PrefixQuery(term);
-
 // Meh
-PhraseQuery query = new PhraseQuery();
-query.add(new Term("title","10"));
-query.add(new Term("title","items"));
+//PhraseQuery query = new PhraseQuery();
+//query.add(new Term("title","10"));
+//query.add(new Term("title","items"));
+
+// WORKS
+//QueryParser queryParser = new QueryParser("title",new StandardAnalyzer());
+//Query query = queryParser.parse("\"10 items or less\"~1");
 
 // This WORKS
 //PhraseQuery query = new PhraseQuery();
@@ -49,8 +82,8 @@ query.add(new Term("title","items"));
 
 
 // Rob query WORKS
-//QueryParser queryParser = new QueryParser("authors",new StandardAnalyzer());
-//Query query = queryParser.parse("Rob");
+QueryParser queryParser = new QueryParser("authors",new StandardAnalyzer());
+Query query = queryParser.parse("Rob");
 	
 
 
@@ -62,18 +95,8 @@ query.add(new Term("title","items"));
 	    }
 		
 		
-//		Hits hits = indexsearcher.search(query);
-//		int hitCount = hits.length();
-//		System.out.println("Results: " + hitCount);
-// 		for (int i = 0; i < hitCount; i++) {
-//                Document doc = hits.doc(i);
-//                System.out.println("  " + (i + 1) + ". " + doc.get("title"));
-//            }
-//		
-		//Hits hits = indexsearcher.search(query);
         System.out.println("Query: " + query);
 		System.out.println("Results: " + results.totalHits);
-//		System.err.println("Done");
 	
 	}
 	
---
+++ starter/src/cs276/pe1/lucene/RunQueries.java
@@ -0,0 +1,28 @@
+package cs276.pe1.lucene;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import cs276.util.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class RunQueries {
+
+
+	public static void main(String[] argv) throws Exception {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        while (true) {
+            System.out.print("Enter query: ");
+            String query = br.readLine();
+            System.out.println(IMDBReader.runQueryForTitle(query));
+        }
+	}
+}

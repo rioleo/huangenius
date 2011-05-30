b76f599f04be00946af8244239ebc0080fb90837
Joseph Lau <enwar3@gmail.com>
2011-05-30 00:34:22 -0700

    test.sh

--- pe2-2011/test.sh
+++ pe2-2011/test.sh
@@ -19,18 +19,13 @@
 #Current accuracies:
 #Binomial: 84.8%
 #Binomial w/ Chi2: 87.0%
-#100, 200, 400, 600, 800, 1600: 342, 341, 351, 344, 344, 340
+#K=100, 200, 400, 600, 800, 1600: 342, 341, 351, 344, 344, 340
 #Multinomial: 96.5%
 #Multinomial w/ Chi2: 91.8%
-#100, 200, 400, 600, 800, 1600: 344, 359, 369, 368, 370, 378
-#Binomial w/ Kfold=10: 77.6%
-#Multinomial w/ Kfold=10: 89.7%
-#Binomial w/ Kfold, Chi2: 83.1%
-#Multinomial w/ Kfold, Chi2: 85.5%
+#K=100, 200, 400, 600, 800, 1600: 344, 359, 369, 368, 370, 378
 #CNB: 384
 #wCNB: 386
-#CNB w/ Kfold=10: 89.6
-#WCNB w/ Kfold=10: 89.6
+
 
 
 #RANDOM KFOLDING:
